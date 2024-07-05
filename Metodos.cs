ae9d569cd7cfdd166b50658f98265da21be91333
Guilherme santos <163909949+guilhermesantos78@users.noreply.github.com>
2024-07-05 16:16:08 -0300

    adicionando mais metodos

--- Metodos.cs
+++ Metodos.cs
@@ -28,6 +28,41 @@ namespace ListaAtividades
             Console.WriteLine($"seu dinheiro convertendo Dolar :{Valor}$ em Real ficará em {resultado}$");
         }
 
+        public void Valores()
+        {
+            Console.WriteLine("Digite um valor");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um segundo valor");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Antes da troca: a = {a}, b = {b}");
+
+            int temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine($"Depois da troca: a = {a}, b = {b}");
+        }
+
+        public void Modulo()
+        {
+            Console.WriteLine("Digite um numero");
+            double Modulo1 = double.Parse(Console.ReadLine());
+
+            if(Modulo1 == 0)
+            {
+                Console.WriteLine($"O numero {Modulo1} é igual 0 ");
+            }
+            else if (Modulo1 > 0)
+            {
+                Console.WriteLine($"O numero {Modulo1} é maior que 0 ");
+            }
+            else
+            {
+                Console.WriteLine($"O numero {Modulo1} é menor que 0 ");
+            }
+        }
+
         }
     }
 
--- Program.cs
+++ Program.cs
@@ -2,32 +2,41 @@
 using System;
 
 Metodos m = new();
-//1
-System.Console.WriteLine("Digite a quantidade Maxima do estoque");
-double Max = double.Parse(Console.ReadLine());
+////1
+//System.Console.WriteLine("Digite a quantidade Maxima do estoque");
+//double Max = double.Parse(Console.ReadLine());
 
-System.Console.WriteLine("Digite a quantidade Minima do estoque");
-double Min = double.Parse(Console.ReadLine());
+//System.Console.WriteLine("Digite a quantidade Minima do estoque");
+//double Min = double.Parse(Console.ReadLine());
 
-m.CalcularEstoqueMedio(Max, Min);
+//m.CalcularEstoqueMedio(Max, Min);
 
 
-//2
+////2
 
-m.DolarReal();
+//m.DolarReal();
 
-//3
-Peca peca1 = new Peca { Codigo = "001", PrecoUnitario = 50.0, Quantidade = 10 };
-Peca peca2 = new Peca { Codigo = "002", PrecoUnitario = 30.0, Quantidade = 15 };
-Peca peca3 = new Peca { Codigo = "003", PrecoUnitario = 40.0, Quantidade = 8 };
+////3
+//Peca peca1 = new Peca { Codigo = "001", PrecoUnitario = 50.0, Quantidade = 10 };
+//Peca peca2 = new Peca { Codigo = "002", PrecoUnitario = 30.0, Quantidade = 15 };
+//Peca peca3 = new Peca { Codigo = "003", PrecoUnitario = 40.0, Quantidade = 8 };
 
-Vendedor vendedor1 = new Vendedor("V001");
-Vendedor vendedor2 = new Vendedor("V002");
+//Vendedor vendedor1 = new Vendedor("V001");
+//Vendedor vendedor2 = new Vendedor("V002");
 
-vendedor1.RealizarVenda(peca1);
-vendedor1.RealizarVenda(peca2);
-vendedor2.RealizarVenda(peca2);
-vendedor2.RealizarVenda(peca3);
+//vendedor1.RealizarVenda(peca1);
+//vendedor1.RealizarVenda(peca2);
+//vendedor2.RealizarVenda(peca2);
+//vendedor2.RealizarVenda(peca3);
 
-Console.WriteLine($"Comissão do vendedor {vendedor1.Identificacao}: R${vendedor1.CalcularComissao()}");
-Console.WriteLine($"Comissão do vendedor {vendedor2.Identificacao}: R${vendedor2.CalcularComissao()}");
+//Console.WriteLine($"Comissão do vendedor {vendedor1.Identificacao}: R${vendedor1.CalcularComissao()}");
+//Console.WriteLine($"Comissão do vendedor {vendedor2.Identificacao}: R${vendedor2.CalcularComissao()}");
+
+
+////4
+
+//m.Valores();
+
+//5
+
+m.Modulo();
